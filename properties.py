85239338f4a084c7ceb7ea75c650154d73605f95
Harshad Ranganathan <32612787+HarshadRanganathan@users.noreply.github.com>
2022-05-11 21:45:50 +0100

    Create properties.py

---
+++ properties.py
@@ -0,0 +1,10 @@
+# Org
+ORG_NAME = 
+
+GITTOKEN = ''
+SEARCH_GIT_REPOS_URL = 'https://github.com/api/v3/search/repositories?'
+GITHUB_BASE_URL = "https://github.com/api/v3"
+SEARCH = "/search"
+ISSUES_AND_PRS_RESOURCE = "/issues"
+QUERY="?"
+OPEN_PR_QUERY = "type:pr+org:{org_name}+state:open+review:required+involves:+involves:+author:+author:&sort=created&order=desc"
